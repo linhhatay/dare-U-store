e89a91f53844cd74eeb5e6b45542ed57589b81f9
Linh <linhhatay2404@gmail.com>
2023-04-13 16:42:38 +0700

    authentication, authorization

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const userController = require("../controllers/userController");
 
 router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+
+router.post("/forgotPassword", authController.forgotPassword);
+router.patch("/resetPassword/:token", authController.resetPassword);
+
+router.patch(
+  "/updateMyPassword",
+  authController.protect,
+  authController.updatePassword
+);
+
+router.patch("/updateMe", authController.protect, userController.updateMe);
+router.delete("/deleteMe", authController.protect, userController.deleteMe);
 
 module.exports = router;
